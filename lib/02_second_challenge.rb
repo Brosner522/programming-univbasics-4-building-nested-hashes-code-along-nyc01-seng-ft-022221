604df1e7fde345ddc641c37fad28bec0d7405ac2
Barak Rosner <rosnerbb@gmail.com>
2021-02-07 02:51:54 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,13 +1,14 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {
-      #your key/value pairs here!
-      }}, 
-    :capulet => {
-      :matriarch => {
-      #your key/value pairs here!
-      }}
-
-  
-end
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
